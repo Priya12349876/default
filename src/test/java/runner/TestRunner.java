97209f8e625a7f7805dc82de895b8eff981a9f6a
Priya12349876 <mounikapriyapyla@gmail.com>
2020-12-10 10:26:27 +0530

    comments

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,10 +6,12 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
- //@RunWith(Cucumber.class)
+ @RunWith(Cucumber.class)
  @CucumberOptions(
 		features = "Feature"
+		
 		,glue={"stepDefinition"}
+		,plugin = {"json:target/cucumber.json"}
 		,tags={"@LoginTest"}
 		//,monochrome=true
 		)
